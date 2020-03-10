b4ab621a979e0b6b66c748ddee1e7fcf5ef23ad0
zeasin <280645618@qq.com>
2020-03-10 11:26:48 +0800

    利用nedb保存自选股功能

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # stock-financing-diary
 股票理财日记本
+
+### 相关库使用说明
++ axios html请求ajxa
++ nedb 数据保存
+
+### 相关接口使用说明
++ 个股详情
+```
+http://img1.money.126.net/data/hs/time/today/1399001.json #网易个股
+```
--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const { app, BrowserWindow,Menu } = require('electron')
 const path = require('path')
 const url = require('url')
 const ipc = require('electron').ipcMain
+// 加载模块
+const nedb = require('nedb');
 
 // const ipc = ipcMain
 let newwin;
@@ -72,3 +74,93 @@ app.on('ready', () => {
   // console.log('获取当前运行目录')
   // console.log(exePath);
 });
+
+//监听添加zixuangu
+ipc.on('addStock',(event, arg) => {
+  
+  // console.log(JSON.stringify(arg))
+  // console.log(arg.code)
+  // console.log(arg['code'])
+  // var stockJson = JSON.parse(arg)//eval('('+arg+')')
+  // console.log(stockJson)
+  // var stockJson = eval(arg)
+  // console.log('板块：'+arg.plate)
+  // console.log()
+  //http://img1.money.126.net/data/hs/time/today/1399001.json
+
+  //加载plate数据
+  const Datastore = require('nedb')
+
+
+
+  if(arg.plate != ''){
+    const plateDb = new Datastore({
+      filename: './data/plate.db',
+      autoload: true
+    });
+    // console.log('板块：')
+    //查询板块是否存在，不存在添加
+    plateDb.findOne({
+      name: arg.plate
+      }, (err, ret) => {
+        if(ret == null){
+          //空数据，插入数据
+          let plate = {
+            name: arg.plate,
+            time: new Date()
+          }
+
+          plateDb.insert(plate, function (err, newDoc) {   // Callback is optional
+            // newDoc is the newly inserted document, including its _id
+            // newDoc has no key called notToBeSaved since its value was undefined
+          });
+        }
+          // console.log(err);
+          // console.log(ret);
+      });
+  }
+  
+  if(arg.code != ''){
+    const stockDb = new Datastore({
+      filename: './data/stock.db',
+      autoload: true
+    });
+
+    stockDb.findOne({
+      symbol: arg.code
+      }, (err, ret) => {
+        if(ret == null){
+          //空数据，插入数据
+          var stock = {
+            symbol:arg.code,
+            name:arg.name,
+            plate:arg.plate,
+            yestclose:arg.price,
+            date:arg.date,
+            lastVolume:arg.lastVolume,
+            comment:arg.comment,
+            time:new Date()
+          }
+
+          stockDb.insert(stock, function (err, newDoc) {   // Callback is optional
+            // newDoc is the newly inserted document, including its _id
+            // newDoc has no key called notToBeSaved since its value was undefined
+          });
+
+        }else{
+          console.log('已经存在'+JSON.stringify(ret));
+        }
+      }
+    )
+  }
+
+
+  // const request = net.request({
+  //   method: 'GET',
+  //   protocol: 'http:',
+  //   hostname: 'img1.money.126.net',
+  //   port: 443,
+  //   path: '/data/hs/time/today/1399001.json'
+  // })
+
+})
